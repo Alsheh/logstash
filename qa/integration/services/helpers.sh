c5072b6e03c4c2366113c2c6c4738e2c38161e5f
Rob Bavey <rob.bavey@elastic.co>
2020-05-28 17:07:07 -0400

    [Test] Replace use of `nc` in `wait_for_port` function
    Fixture test scripts use `nc` to wait for the port to determine
    whether a test fixture is up and running. This commit replaces the
    use of `nc` with `lsof`, as `nc` is not installed on Jenkins centos
    worker nodes.

--- qa/integration/services/helpers.sh
+++ qa/integration/services/helpers.sh
@@ -14,13 +14,13 @@ setup_install_dir() {
 wait_for_port() {
     count=$PORT_WAIT_COUNT
     port=$1
-    while ! nc -z localhost $port && [[ $count -ne 0 ]]; do
+    while ! lsof -i :$port && [[ $count -ne 0 ]]; do
         count=$(( $count - 1 ))
         [[ $count -eq 0 ]] && return 1
         sleep 0.5
     done
     # just in case, one more time
-    nc -z localhost $port
+    lsof -i :$port
 }
 
 clean_install_dir() {
